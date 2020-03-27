a8c25d606890d6454b2ab6dd39483a117308009b
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-27 21:03:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,60 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles' do
+    
+    @articles = Article.all
+    
+    erb :index
+  end
+
+  get '/articles/new' do
+    
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  
+
+  
+  post '/articles' do
+    user_title = params[:title]
+    user_content = params[:content]
+    
+    Article.create(:title => user_title, :content => user_content)
+    
+    redirect "/articles/#{Article.all.last.id}"
+    
+    
+  end
+  
+  
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/models/:id' do
+    @article = Article.find(params[:id])
+    @article.name = params[:name]
+    @article.content = params[:content]
+    @article.save
+    
+    redirect "/articles/#{params[:id]}"
+  end
+  
+  
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect "/articles"
+  end
+
 end
