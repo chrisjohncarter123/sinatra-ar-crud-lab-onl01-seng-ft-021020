7ecda36e3e7078c46c92fc203a46225c48c4db11
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-27 21:10:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do
     article = Article.find(params[:id])
-    article.name = params[:name]
+    article.title = params[:title]
     article.content = params[:content]
     article.save
     
